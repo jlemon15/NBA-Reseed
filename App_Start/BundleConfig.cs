48331842882d608ffa952719c32da52cf8c03e88
Jason Lemon <jlemon22@gmail.com>
2020-03-30 21:36:28 -0500

    Modal Work

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -20,7 +20,9 @@ namespace NBA_Reseed
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                 "~/Scripts/umd/popper.js",
+                "~/Scripts/bootstrap.js"
+                      ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/CSS/bracket_SemiFinal.css",
